228551cc43af299bc14508bac841e1fb3ccd1ed9
Samemantlt <46570878+Samemantlt@users.noreply.github.com>
2019-08-25 00:43:01 +0400

    ШОК 2

---
+++ shokbeta/offline.js
@@ -0,0 +1,19 @@
+﻿{
+	"version": 1566679033,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"images/govno-sheet0.png",
+		"images/player-sheet0.png",
+		"images/arrowright-sheet0.png",
+		"images/shopbutton-sheet0.png",
+		"images/background.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
